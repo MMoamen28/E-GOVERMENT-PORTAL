cb893a67b4b843913561bcbe7484f1b652d49a7f
fadyadell <fady3718@gmail.com>
2026-03-14 12:26:46 +0200

    setting up docker and keycloak and databse

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'egov_user',
+      password: 'egov_pass',
+      database: 'e_gov_portal',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
