66c509c5022270206d7cf8b3b07ee936085ad29c
yassineeldaly22 <yassineeldaly02@gmail.com>
2026-03-15 04:54:47 +0200

    feat: complete Flowable task service for officer approvals (KAN-46)

--- src/business-license/business-license.module.ts
+++ src/business-license/business-license.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GoRulesService } from './gorules.service';
 import { FlowableService } from './flowable.service';
+import { FlowableTaskService } from './flowable-task.service';
 
 @Module({
-  providers: [GoRulesService, FlowableService],
-  exports: [GoRulesService, FlowableService],
+  providers: [GoRulesService, FlowableService, FlowableTaskService],
+  exports: [GoRulesService, FlowableService, FlowableTaskService],
 })
 export class BusinessLicenseModule {}
