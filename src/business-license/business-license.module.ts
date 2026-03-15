0e35ed5f655e7b35b20527409444b3b7e8b828e2
yassineeldaly22 <yassineeldaly02@gmail.com>
2026-03-15 04:24:50 +0200

    feat: add FlowableService and update module

--- src/business-license/business-license.module.ts
+++ src/business-license/business-license.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { GoRulesService } from './gorules.service';
-
+import { FlowableService } from './flowable.service';
 
 @Module({
-  
-  providers: [GoRulesService], 
+  providers: [GoRulesService, FlowableService],
+  exports: [GoRulesService, FlowableService],
 })
 export class BusinessLicenseModule {}
